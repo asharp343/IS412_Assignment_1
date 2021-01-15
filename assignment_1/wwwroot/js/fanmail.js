1381c361be99fe7663499cd84e61675d6710f429
Anthony Sharp <asharp343@gmail.com>
2021-01-14 20:42:01 -0700

    Initial check-in of module assignment_1

---
+++ assignment_1/wwwroot/js/fanmail.js
@@ -0,0 +1,9 @@
+﻿var submit = document.getElementById("submitButton")
+
+submit.addEventListener("click", function () {
+    alert(
+    "From: " + document.getElementById("fromField").value + "\n" +
+    "Email: " + document.getElementById("emailField").value + "\n" +
+    "Subject: " + document.getElementById("subjectField").value + "\n" +
+    "Message: " + document.getElementById("messageField").value)
+});
