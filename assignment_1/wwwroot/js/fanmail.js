67486402390f3a582eee134cd3ead9ac6a1fd2cb
Anthony Sharp <asharp343@gmail.com>
2021-01-14 21:49:28 -0700

    update

--- assignment_1/wwwroot/js/fanmail.js
+++
@@ -1,9 +0,0 @@
-﻿var submit = document.getElementById("submitButton")
-
-submit.addEventListener("click", function () {
-    alert(
-    "From: " + document.getElementById("fromField").value + "\n" +
-    "Email: " + document.getElementById("emailField").value + "\n" +
-    "Subject: " + document.getElementById("subjectField").value + "\n" +
-    "Message: " + document.getElementById("messageField").value)
-});
